925e9a4456bdc24c21579512a2eb7991ee636212
Александр Колтунов <alex@MacBook-Pro-Aleksandr.local>
2022-02-08 20:49:02 +1000

    Подключение к сокетам, создание комнат, пользователей в них, сохранение сообщений

--- index.js
+++ index.js
@@ -3,17 +3,64 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const rooms = require("./db/rooms.js");
 const io = new Server(server);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-io.on('connection', (socket) => {
-    console.log('user connect');
+app.post('/rooms', (req, res) => {
+   const {roomId, userName} = req.body;
+   if(!roomId || !userName) {
+       return res.json({message: 'Не заполнено одно из полей'});
+   }
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+   if(!rooms[roomId]) {
+       rooms[roomId] = {
+           id: roomId,
+           users: [],
+           messages: []
+       }
+   }
+   res.json({message: 'ok', rooms})
+});
+
+app.get('/rooms/:id', (req, res) => {
+   const {id: roomId} = req.params;
+   if(rooms[roomId]) {
+        const data = {users: rooms[roomId].users, messages: rooms[roomId].messages};
+        res.json(data);
+   } else {
+       res.json({users: [], messages: []});
+   }
+});
+
+io.on('connection', (socket) => {
+    socket.on('join', ({roomId, userName}) => {
+        socket.join(roomId);
+        const users = rooms[roomId].users;
+        // const users = Object.values(rooms).filter(room => room.id === roomId)[0].users;
+        users.push({id: socket.id, name: userName});
+
+        socket.broadcast.to(roomId).emit('setUsers', users);
+    });
+
+    socket.on('newMessage', ({roomId, userName, content}) => {
+        const messageContent = {userName, content};
+        rooms[roomId].messages.push(messageContent);
+        socket.broadcast.to(roomId).emit('sendMessage', messageContent);
+    });
+
+    socket.on('disconnect', (reason) => {
+        Object.values(rooms).forEach(room => {
+            const newRoom = {
+                ...room,
+                users: room.users.filter(user => user.id !== socket.id)
+            };
+            rooms[newRoom.id] = newRoom;
+            socket.broadcast.to(newRoom.id).emit('setUsers', newRoom.users);
+        });
     });
 });
 
