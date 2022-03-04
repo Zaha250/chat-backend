fc6dd7641a3e4945c3ae0f87d7c0b84d2c379264
Zaha250 <koltunov1998@mail.ru>
2022-03-04 23:03:41 +1000

    Добавлен пароль при регистрации и авторизации

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,9 +5,13 @@ router.post('/', async (req, res) => {
     try{
         const user = await UserModel.findOne({login: req.body.login}).exec();
         if(user) {
-            res.json({user, error: null});
+            if(user.password === req.body.password) {
+                res.json({user, error: null});
+            } else {
+                res.json({user: null, error: 'Введен неверный логин или пароль'});
+            }
         } else {
-            res.json({user: null, error: 'Пользователь с данным именем не найден'});
+            res.json({user: null, error: 'Введен неверный логин или пароль'});
         }
     } catch (error) {
         res.status(500).json({error});
@@ -15,9 +19,10 @@ router.post('/', async (req, res) => {
 });
 
 router.post('/reg', async (req, res) => {
-    const { login, name, secondName, lastName, photo } = req.body;
+    const { login, name, secondName, lastName, photo, password } = req.body;
     const newUser = new UserModel({
         login,
+        password,
         name,
         secondName,
         lastName,
